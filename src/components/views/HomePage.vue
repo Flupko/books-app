13a60e3a3076d9f94df39d8d3a84e91bcd1c8f21
Flupko <flupko@gmail.com>
2022-05-31 10:38:18 +0200

    Homepage update + prevent zoom on mobile

--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -1,17 +1,21 @@
 <template>
   <q-page>
     <SearchBar> </SearchBar>
-    <BookList :bookList="store.searchResults"> </BookList>
+    <BookList v-if="store.searchResults" :bookList="store.searchResults">
+    </BookList>
+    <AppInfo v-else> </AppInfo>
   </q-page>
 </template>
 <script>
 import SearchBar from "../layout/SearchBar.vue";
 import BookList from "../layout/BookList.vue";
+import AppInfo from "../layout/AppInfo.vue";
 import { useStore } from "../../store/index";
 export default {
   components: {
     SearchBar,
     BookList,
+    AppInfo,
   },
   setup() {
     const store = useStore();
