6d583c7754c99d868d297d2fff8d8d7652a2c9f9
Flupko <flupko@gmail.com>
2022-05-31 10:45:18 +0200

    aaa

---
+++ src/components/layout/AppInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span class="text-h6 q-ma-md">
+      - Open-source application to search books infomations and save it.</span
+    >
+    <span class="text-h6 q-ma-md">
+      - Search for any books you would like, and get details about the author,
+      the plot, ...</span
+    >
+    <span class="text-h6 q-ma-md"> (Working thanks to Google Books API) </span>
+  </div>
+</template>
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+span {
+  text-align: center;
+}
+</style>
